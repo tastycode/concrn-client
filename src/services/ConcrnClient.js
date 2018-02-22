99f738ccdd0a271dd1d0c65c910dbdfd1a7519f5
tastycode <kultiv8tor@gmail.com>
2018-02-22 11:44:14 -0600

    Add prettier automatic linting/formatting

--- src/services/ConcrnClient.js
+++ src/services/ConcrnClient.js
@@ -1,34 +1,36 @@
-import axios from 'axios'
+import axios from "axios"
+import Config from "react-native-config"
 function apiRequest(method, path) {
   return async (data, requestParams) => {
-    const replacedPath = Object.keys(data).reduce( (path, key) => {
+    const replacedPath = Object.keys(data).reduce((path, key) => {
       return path.replace(`:${key}`, data[key])
     }, path)
+    alert(Config.API_URL)
+    const apiUrl = Config.API_URL
     const response = await axios({
       method,
-      url: `http://localhost:3000/api${replacedPath}`,
+      url: `${apiUrl}/api${replacedPath}`,
       data,
-      ...requestParams
+      ...requestParams,
     })
     return response.data
   }
 }
 
-
 export default {
   device: {
-    validate: apiRequest('POST', '/devices/validate'),
-    create: apiRequest('POST', '/devices'),
-    verify: apiRequest('POST', '/devices/verify')
+    validate: apiRequest("POST", "/devices/validate"),
+    create: apiRequest("POST", "/devices"),
+    verify: apiRequest("POST", "/devices/verify"),
   },
   report: {
-    create: apiRequest('POST', '/reports'),
+    create: apiRequest("POST", "/reports"),
     messages: {
-      create: apiRequest('POST', '/reports/:reportId/messages')
-    }
+      create: apiRequest("POST", "/reports/:reportId/messages"),
+    },
   },
   responder: {
-    validate: apiRequest('GET', '/responders/device'),
-    update: apiRequest('PUT', '/responders/:id')
-  }
+    validate: apiRequest("GET", "/responders/device"),
+    update: apiRequest("PUT", "/responders/:id"),
+  },
 }
